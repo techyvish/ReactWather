890f6e343dec3fe03ef5aac4dd740f665308e3bc
Vishal Patel <vis8051@gmail.com>
2016-08-24 04:24:19 +1000

    * Added Foundation

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,9 @@ var Examples = require('Examples');
 
 var { Route, Router, IndexRoute, hashHistory } = require('react-router'); // Object destructing syntex
 
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
